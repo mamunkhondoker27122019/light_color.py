f127095a77dbc7ba1796bd17bfd0ffa83d4d8c0a
mamunkhondoker27122019 <129860130+mamunkhondoker27122019@users.noreply.github.com>
2023-04-19 11:26:21 +0300

    Create light_color.py

---
+++ light_color.py
@@ -0,0 +1,15 @@
+color_name = input("Введите название цвета: ")
+if color_name == "красный":
+    print("Код цвета RGB: (255, 0, 0)")
+elif color_name == "оранжевый":
+    print("Код цвета RGB: (255, 165, 0)")
+elif color_name == "желтый":
+    print("Код цвета RGB: (255, 255, 0)")
+elif color_name == "зеленый":
+    print("Код цвета RGB: (0, 128, 0)")
+elif color_name == "синий":
+    print("Код цвета RGB: (0, 0, 255)")
+elif color_name == "фиолетовый":
+    print("Код цвета RGB: (128, 0, 128)")
+else:
+    print("Этот цвет не распознан.")
